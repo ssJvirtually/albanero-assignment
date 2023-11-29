919e721f8b37d171e1acedd908f2ac7f387dd842
ssJvirtually <jskr456@gmail.com>
2023-11-29 20:02:09 +0530

    refactored shipment

--- shipment/src/main/java/com/albanero/shipment/controller/ShipmentController.java
+++ shipment/src/main/java/com/albanero/shipment/controller/ShipmentController.java
@@ -3,7 +3,11 @@ package com.albanero.shipment.controller;
 import com.albanero.shipment.entity.Shipment;
 import com.albanero.shipment.service.ShipmentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class ShipmentController {
 
 
@@ -14,7 +18,7 @@ public class ShipmentController {
     }
 
     @GetMapping("/orderId")
-    public Shipment getPaymentDetailsByOrderId(Integer orderId){
+    public Shipment getPaymentDetailsByOrderId(@RequestParam("orderId") Integer orderId){
         return shipmentService.getShipmentDetailsByOrderId(orderId);
     }
 }
